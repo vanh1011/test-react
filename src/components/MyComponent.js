379824059eb2eda38d3e4967d461e48487aedadc
unknown <vietanh10112002@gmail.com>
2022-10-28 17:57:13 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class MyComponent extends React.Component {
+    //jsx : cho phep viet code js trong file html
+    render() {
+
+        return (
+            <div> my first component </div>
+        )
+    }
+}
+
+
+export default MyComponent;
